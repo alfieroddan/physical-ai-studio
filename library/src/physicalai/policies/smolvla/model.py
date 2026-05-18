c87877c9f4b92aa8be1ee712724bc43a8d5201cb
Alfie Roddan <51797647+alfieroddan@users.noreply.github.com>
2026-05-18 17:27:26 +0200

    refactor: remove in-place mutation of objects for safety

--- library/src/physicalai/policies/smolvla/model.py
+++ library/src/physicalai/policies/smolvla/model.py
@@ -386,6 +386,7 @@ class SmolVLAModel(ExportableModelMixin, Model):
 
     @staticmethod
     def _pi_aloha_encode_actions(actions: torch.Tensor) -> torch.Tensor:
+        actions = actions.clone()
         # Flip the joints.
         for motor_idx in [1, 2, 8, 9]:
             actions[:, :, motor_idx] *= -1
@@ -396,6 +397,7 @@ class SmolVLAModel(ExportableModelMixin, Model):
 
     @staticmethod
     def _pi_aloha_encode_actions_inv(actions: torch.Tensor) -> torch.Tensor:
+        actions = actions.clone()
         # Flip the joints again.
         for motor_idx in [1, 2, 8, 9]:
             actions[:, :, motor_idx] *= -1
